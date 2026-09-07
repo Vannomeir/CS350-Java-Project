74d6416d3b199778fd3d950bac7f357f5cf8f7e2
Madalitso Kasongo <madalitsokas4@gmail.com>
2026-09-07 15:24:09 +0200

    Initialize ExpenseTracker class with main method

---
+++ ExpenseTracker.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExpenseTracker {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double income = 0;
+        double totalExpenses = 0;
+        int choice;
+}
